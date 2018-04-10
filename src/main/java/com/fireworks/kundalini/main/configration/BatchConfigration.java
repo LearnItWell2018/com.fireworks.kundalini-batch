385ce300462ca6d7dd7a36a77c14fb46127cbbfe
LearnItWell2018 <35795423+LearnItWell2018@users.noreply.github.com>
2018-04-10 17:36:56 +0530

    code new

--- src/main/java/com/fireworks/kundalini/main/configration/BatchConfigration.java
+++ src/main/java/com/fireworks/kundalini/main/configration/BatchConfigration.java
@@ -1,19 +1,67 @@
 package com.fireworks.kundalini.main.configration;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.NonTransientResourceException;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.item.UnexpectedInputException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import com.fireworks.kundalini.main.resource.CustomerOrder;
 
 @Configuration
 @EnableBatchProcessing
+@ComponentScan("com.fireworks.kundalini")
+@PropertySource("batch.properties")
 public class BatchConfigration {
-	@Autowired
+       @Autowired
     public JobBuilderFactory jobBuilderFactory;
 
     @Autowired
     public StepBuilderFactory stepBuilderFactory;
 
+    @Bean
+       public Job importUserJob() {
+              return jobBuilderFactory.get("importUserJob")
+                           .incrementer(new RunIdIncrementer())
+                           .start(stepPDF()).next(stepMail()).build();
+       }
+
+       private Step stepMail() {
+              return stepBuilderFactory.get("stepRead").tasklet(getResourceDetails()).build();
+       }
+
+       private Tasklet getResourceDetails() {
+              return null;
+       }
+
+       private Step stepPDF() {
+              return stepBuilderFactory.get("stepPDF").<String, CustomerOrder>chunk(1).reader( new ItemReader<String>() {
+
+                     public String read() throws Exception, UnexpectedInputException,
+                                  ParseException, NonTransientResourceException {
+                           // TODO Auto-generated method stub
+                           return null;
+                     }
+              }).processor(new ItemProcessor<String, CustomerOrder>() {
+
+                     public CustomerOrder process(String item) throws Exception {
+                           // TODO Auto-generated method stub
+                           return null;
+                     }
+              }).build();
+       }
+    
     
 }
